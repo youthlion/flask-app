3706041a4e2e46b3be0ccf40e1ef39783bb76c7f
youthlion <2877402302@qq.com>
2018-05-18 23:46:18 +0800

    bootstrap模板以及一些基本的html文件

---
+++ app.py
@@ -0,0 +1,17 @@
+#/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html',name=name)
+
+if __name__=="main":
+	app.run(debug=True)
