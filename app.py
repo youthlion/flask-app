cc0fec69a79eb21dbfc1a9fa1739c6bc3efece47
youthlion <2877402302@qq.com>
2018-05-20 10:41:02 +0800

    upload

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 #/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from flask import Flask,render_template
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
 
 
 app=Flask(__name__)
 
-print('Hello,world')
+bootstrap=Bootstrap(app)
+
 
 @app.route('/')
 def index():
@@ -16,6 +19,10 @@ def index():
 def user(name):
 	return render_template('user.html',name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
 
-if __name__=="main":
+
+if __name__=="__main__":
 	app.run(debug=True)
