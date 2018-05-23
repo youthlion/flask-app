0428fb6c0ab55ff1316aecd6b1e1ba6c1e91aebf
youthlion <2877402302@qq.com>
2018-05-23 15:26:26 +0800

    表单初步学习

--- app.py
+++ app.py
@@ -4,6 +4,9 @@
 from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
 
 app=Flask(__name__)
@@ -13,16 +16,11 @@ bootstrap=Bootstrap(app)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template("index.html")
 
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html',name=name)
 
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html'),404
-
-
-if __name__=="__main__":
+if __name__=='__main__':
 	app.run(debug=True)
